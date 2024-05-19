4f582d9cdc58cf3176dc6b8977f9f1746c775397
rodolfojesilva <rodolfoti.jesus@gmail.com>
2024-05-19 14:34:46 -0300

    adiciona metodos de somar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Calculadora\nInforme os valores");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("1-Somar");
+string op = Console.ReadLine()!;
+
+switch (op){
+    case "1":
+        Somar(a, b);
+        break;
+    case "0":
+        return;
+    default:
+        Console.WriteLine("opcao nao encontrada");
+        break;
+}
+
+
+static void Somar(int a, int b) =>
+    Console.WriteLine($"Resultado: {a + b}");
